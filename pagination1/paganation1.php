1964eb8f4f000633bf62078f22160f25b9ef8bb1
shuto niimi <71388489+ooitanojohn@users.noreply.github.com>
2021-06-20 11:58:34 +0900

    修正完了 mvc化する

--- pagination1/paganation1.php
+++ pagination1/paganation1.php
@@ -1,15 +1,16 @@
 <?php
 
-define('MAX', '3');
-$books = array(
-    array('book_kind' => 'ライトノベル', 'book_name' => 'ライトノベルの本'),
-    array('book_kind' => '歴史', 'book_name' => '歴史の本'),
-    array('book_kind' => '料理', 'book_name' => '料理の本'),
-    array('book_kind' => '啓発本', 'book_name' => '啓発の本'),
-    array('book_kind' => 'コミック', 'book_name' => 'コミックの本'),
-    array('book_kind' => '推理小説', 'book_name' => '推理小説の本'),
-    array('book_kind' => 'フォトブック', 'book_name' => 'フォトブックの本'),
-);
+const MAX = '3';
+
+$books = [
+    ['book_kind' => 'ライトノベル', 'book_name' => 'ライトノベルの本'],
+    ['book_kind' => '歴史', 'book_name' => '歴史の本'],
+    ['book_kind' => '料理', 'book_name' => '料理の本'],
+    ['book_kind' => '啓発本', 'book_name' => '啓発の本'],
+    ['book_kind' => 'コミック', 'book_name' => 'コミックの本'],
+    ['book_kind' => '推理小説', 'book_name' => '推理小説の本'],
+    ['book_kind' => 'フォトブック', 'book_name' => 'フォトブックの本'],
+];
 
 $books_num = count($books);
 $max_page = ceil($books_num / MAX);
--- readme.md
+++ readme.md
@@ -1,13 +1,13 @@
 # pagenation
 
-様々な奴(php,js)見比べて理解、カスタマイズ、利用できる様にするのが目的。
+様々な奴(php,js)見比べて理解、カスタマイズ、利用できる様にするのが目的。ノッてくるまで markdown、git の練習も兼ねるとする
 
 ## pagenation1
 
 [侍エンジニア](https://www.sejuku.net/blog/70234)
 
 - 変更する所(予定)
-  - define から const へ
-  - array を [] に
+  - ~~define から const へ~~
+  - ~~array を [] に~~
   - 変数名をどうするか。
   - そもそもコピペだと動いていないので動くようにする and mvc 化
