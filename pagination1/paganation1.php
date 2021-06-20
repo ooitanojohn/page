e0627679e8ee028899f134c2a4819b23f433cc3f
shuto niimi <71388489+ooitanojohn@users.noreply.github.com>
2021-06-20 11:41:11 +0900

    first commit

---
+++ pagination1/paganation1.php
@@ -0,0 +1,42 @@
+<?php
+define('MAX', '3');
+$books = array(
+    array('book_kind' => 'ライトノベル', 'book_name' => 'ライトノベルの本'),
+    array('book_kind' => '歴史', 'book_name' => '歴史の本'),
+    array('book_kind' => '料理', 'book_name' => '料理の本'),
+    array('book_kind' => '啓発本', 'book_name' => '啓発の本'),
+    array('book_kind' => 'コミック', 'book_name' => 'コミックの本'),
+    array('book_kind' => '推理小説', 'book_name' => '推理小説の本'),
+    array('book_kind' => 'フォトブック', 'book_name' => 'フォトブックの本'),
+);
+
+
+$books_num = count($books);
+$max_page = ceil($books_num / MAX);
+
+if (!isset($_GET['page_id'])) {
+    $now = 1;
+} else {
+    $now = $_GET['page_id'];
+}
+
+$start_no = ($now - 1) * MAX;
+
+$disp_data = array_slice($books, $start_no, MAX, true);
+
+
+?>
+
+foreach($disp_data as $val){
+echo $val['book_kind']. '　'.$val['book_name']. '<br />';
+}
+
+echo '全件数'. $books_num. '件'. '　'; // 全データ数の表示です。
+
+if($now > 1){ // リンクをつけるかの判定
+echo '<a href='/paging.php?page_id=' .($now - 1).'')>前へ</a>'. '　';
+} else {
+echo '前へ'. '　';
+}
+
+for($i = 1; $i <= $max_page; $i++){ if ($i==$now) { echo $now. '　' ; } else { echo '<a href=' /test.php?page_id='. $i. '')>' . $i. '</a>' . '　' ; } } if($now < $max_page){ // リンクをつけるかの判定 echo '<a href=' /paging.php?page_id='.($now + 1).'')>次へ</a>' . '　' ; } else { echo '次へ' ; }
---
+++ pagination1/readme.md
@@ -0,0 +1,3 @@
+# https://www.sejuku.net/blog/70234
+
+pagenation 様々な奴(php,js)見比べて理解、カスタマイズ、利用できる様にするのが目的。
